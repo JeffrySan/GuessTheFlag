4c8f2d873472f46c8ca8b428210346a11d807ebb
Jeffry Sandy <jeffry.sandy@dana.id>
2024-11-30 19:21:18 +0700

    custom button

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,15 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+	
 	var body: some View {
 		VStack {
-			Text("Your content")
-				.frame(maxWidth: .infinity, maxHeight: .infinity)
-				.foregroundStyle(.white)
-				.background(.red.gradient)
-				.frame(width: .infinity, height: 500, alignment: .bottom)
+			Button("Button 1") { }
+				.buttonStyle(.bordered)
+			Button("Button 2", role: .destructive) { }
+				.buttonStyle(.bordered)
+			Button("Button 3") { }
+				.buttonStyle(.borderedProminent)
+			Button("Button 4", role: .destructive) { }
+				.buttonStyle(.borderedProminent)
 		}
-		.ignoresSafeArea()
+	}
+	
+	func executeDelete() {
+		print("Now deleting…")
 	}
 }
 
