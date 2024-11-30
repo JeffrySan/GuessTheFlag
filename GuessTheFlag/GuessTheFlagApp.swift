8d4a034821e9e8c380f58751b1f73e14e8c34875
Jeffry Sandy <jeffry.sandy@dana.id>
2024-11-30 15:02:38 +0700

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Jeffry Sandy Purnomo on 30/11/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
